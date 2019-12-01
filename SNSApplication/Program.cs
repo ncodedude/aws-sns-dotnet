189ddf95425af74a5a1c63c7474eda6e02cef2ff
unknown <ncodedude@gmail.com>
2019-12-01 14:51:58 -0300

    Inicial Commit

---
+++ SNSApplication/Program.cs
@@ -0,0 +1,26 @@
+﻿using Amazon.SimpleNotificationService;
+using Amazon.SimpleNotificationService.Model;
+using System;
+using System.Threading.Tasks;
+
+namespace SNSApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            AmazonSimpleNotificationServiceClient client = new AmazonSimpleNotificationServiceClient("", "", Amazon.RegionEndpoint.USWest2);
+
+            PublishRequest request = new PublishRequest();
+            request.Message = "Seu número da sorte: Z4T568TUYA";
+            request.PhoneNumber = "+55";
+
+            var result = client.PublishAsync(request).Result;
+
+            Console.WriteLine("Mensagem enviada!");
+            Console.ReadKey();
+        }
+    }
+}
